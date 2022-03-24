d13e1fc0156c76f498237ec918348b453ee925f0
NielAndalecio <andalecionieljohn@gmail.com>
2022-03-24 20:52:18 +0800

    Update survey.php

--- app/Views/survey.php
+++ app/Views/survey.php
@@ -14,30 +14,109 @@
         <input type="text" name="name" id="name" placeholder="name">
         <br>
         PART I
-        1. DEMOGRAPHIC PROFILE OF RESPONDENT
+        DEMOGRAPHIC PROFILE OF RESPONDENT
         <br>
-        1.1 Age: 
+        Age: 
         <input type=radio name="age" id="a"> 20-29 yrs. old
         <input type=radio name="age" id="b"> 30-39 yrs. old
         <input type=radio name="age" id="c"> 40-49 yrs. old
         <input type=radio name="age" id="d"> 50-59 yrs. old
         <input type=radio name="age" id="e"> 60 yrs old and above yrs. old
         <br>
-        1.2 Sex:
+        Sex:
         <input type=radio name="gender" id="male"> Male
         <input type=radio name="gender" id="female"> Female
         <br>
-        1.3 Civil Status
+        Civil Status
         <input type=radio name="civil" id="single"> Single
         <input type=radio name="civil" id="married"> Married
         <input type=radio name="civil" id="widow"> Widow/Widower
         <input type=radio name="civil" id="legalsep"> Legally Separated
         <input type=radio name="civil" id="annulled"> Marriage Annulled
         <input type=radio name="civil" id="sepinfact"> Separated in fact
-        <input type=radio name="civil" id="sepinfact"> Separated in fact
+        <br>
+        Ethnicity
+        <input type=radio name="eth" id="tag"> Tagalog
+        <input type=radio name="eth" id="ilok"> Ilokano
+        <input type=radio name="eth" id="kap"> Kapampangan
+        <input type=radio name="eth" id="bik"> Bikolano
+        <input type=radio name="eth" id="aet"> Aeta
+        <input type=radio name="eth" id="igo"> Igorot
+        <input type=radio name="eth" id="iva"> Ivatan
+        <input type=radio name="eth" id="man"> Mangyan
+        <br>
+        <input type=radio name="eth" id="ilon"> Ilonggo
+        <input type=radio name="eth" id="ati"> Ati
+        <input type=radio name="eth" id="sa;"> Saludnon
+        <input type=radio name="eth" id="ceb"> Cebuano
+        <input type=radio name="eth" id="war"> waray
+    
+        <br>
+        <input type=radio name="eth" id="bad"> Badjao
+        <input type=radio name="eth" id="yak"> Yakan
+        <input type=radio name="eth" id="bla"> B'laan
+        <input type=radio name="eth" id="tbo"> T'boli
+        <input type=radio name="eth" id="tau"> Tausag
+        <input type=radio name="eth" id="bag"> Bagobo
+        <input type=radio name="eth" id="mar"> Maranao
+        <br>
+        Number of Children
+        <input type=radio name="children" id="zero"> 0 child
+        <input type=radio name="children" id="one"> 1 child
+        <input type=radio name="children" id="two"> 2 children
+        <input type=radio name="children" id="three"> 3 children
+        <input type=radio name="children" id="four"> 4 children
+        <input type=radio name="children" id="five"> 5 or more children
+        <input type=radio name="children" id="notappli"> Not applicable
+        <br>
+        Highest Educational Attainment
+        <input type=checkbox name="rangechild" id="range-2"> below 2 yrs. old
+        <input type=checkbox name="rangechild" id="range-5"> 2-5 yrs. old
+        <input type=checkbox name="rangechild" id="range-12"> 6-12 yrs. old 
+        <input type=checkbox name="rangechild" id="range-18"> 13-18 yrs. old 
+        <input type=checkbox name="rangechild" id="range-19"> 19 yrs. old and above 
+        <input type=checkbox name="rangechild" id="range-notappli"> Not applicable 
+        <br>
+        Highest Educational Attainment
+        <input type=radio name="education" id="college"> College Graduate
+        <input type=radio name="education" id="post"> College Graduate
+        <input type=radio name="education" id="masters"> College Graduate
+        <input type=radio name="education" id="doctorate"> College Graduate
+        <br>
+        Area of Specialization (Please check all applicable answer):
+        <input type=checkbox name="specialization" id="laws"> Laws and Jurisprudence 
+        <input type=checkbox name="specialization" id="pubsafe"> Public Safety/National Security (e.g. Police/Fire/Jail Fields of Discipline)
+        <input type=checkbox name="specialization" id="educ"> Education and Research 
+        <input type=checkbox name="specialization" id="sci"> Sciences (e.g. Physical, Natural, Social)
+        <input type=checkbox name="specialization" id="leadership"> Leadership and Management
+        <input type=checkbox name="specialization" id="oth"> Others, specify:  
+        <input type=textbox name="specialization" id="others">
         <br>
 
+        Employment Status of Spouse/Partner
+        <input type=radio name="employmentpartner" id="emp"> Employed
+        <input type=radio name="employmentpartner" id="unemp"> Unemployed
+        <input type=radio name="employmentpartner" id="empnotappli"> Not applicable
+        
+        If spouse/partner is employed, please check monthly salary bracket:
+        <input type=radio name="salbracket" id="20k"> below P20,000.00
+        <input type=radio name="salbracket" id="40k"> P20,000.00 - P40,000.00
+        <input type=radio name="salbracket" id="60k"> P40,001.00 - P60,000.00
+        <input type=radio name="salbracket" id="80k"> P60,001.00 - P80,000.00
+        <input type=radio name="salbracket" id="100k"> P80,001.00 - P100,000.00
+        <input type=radio name="salbracket" id="200k"> Above P100,000.00
 
-        </form>
+        Employment
+
+        Current Affiliation
+        <input type=radio name="affil" id="PNP"> Philippine National Police				
+        <input type=radio name="affil" id="BJMP"> Bureau of Jail Management and Penology	
+        <input type=radio name="affil" id="BFP"> Bureau of Fire Protection
+        <input type=radio name="affil" id="othgov"> Other Government Agency/Institution, specify:   
+        <input type=textbox name="affiltext" id="others">
+        <input type=radio name="affil" id="othpriv"> Private Agency/Institution, specify: 
+        <input type=textbox name="affiltext" id="others">
+
+    </form>
     </body>
 </html>
