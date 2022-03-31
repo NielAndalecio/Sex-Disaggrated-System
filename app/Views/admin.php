2ec2f3d0bb529fe3047cbd3f6c1838ffc92e3e4b
NielAndalecio <andalecionieljohn@gmail.com>
2022-03-31 17:21:55 +0800

    zzxz
    xzx

---
+++ app/Views/admin.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title>
+            Sex-Dissagrated Data Survey - Philippine Public Safety College
+        </title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <form>
+            Topic: <input type="text" id="topic"><br>
+            <button> Add group </button> <button> Add Question </button> <br>
+            Add question <input type="text" id="ques"> 
+            parent <select id="parent">
+                        <option value="none" default>----none----</option>
+                    </select>
+            choice type <select id="choice">
+                        <option value="radio">Single</option>
+                        <option value="checkbox">Multiple</option>
+                    </select> <br>
+            <button> Add Choice </button> <br>
+            Choice: <input type="text">
+            Choice: <input type="text">
+
+
+        </form>    
+    </body>
+</html>
